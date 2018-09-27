67b15d41c0752607da9bd0880152f26c6b20c7b4
木槿科技 <sunyuandong@mujinkeji.com>
2018-09-27 17:55:00 +0800

    1.上传table和form表单

---
+++ src/Vue/vue01.vue
@@ -0,0 +1,19 @@
+<template>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js">
+
+    </script>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "vue01"
+    }
+</script>
+
+<style scoped>
+
+</style>
+
